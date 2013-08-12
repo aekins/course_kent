bb27821598dcff53d3221d6d0e97a47d6b6ebfd5
aekins <andy@aekins.co.uk>
2013-08-12 15:17:02 +0100

    Update access.php

--- db/access.php
+++ db/access.php
@@ -14,13 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * course_kent
- *
- * @package    block_course_kent
- * @copyright  2013 Andy Ekins
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+/*
+* Capabilities
+*
+* @package block_course_kent
+* @category navigation
+* @copyright 2013 Andy Ekins
+* @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
 
 defined('MOODLE_INTERNAL') || die();
 
